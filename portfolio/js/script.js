de6aeaea229e6580f7654d7ec1a1c8f66753c813
MariuszWSEI <mariusz.krainski@microsoft.wsei.edu.pl>
2025-04-14 20:39:36 +0200

    Delete portfolio directory

--- portfolio/js/script.js
+++
@@ -1,8 +0,0 @@
-const nav = document.querySelector('.nav-items');
-const navBtn = document.querySelector('.bars');
-
-const handleNav = () => {
-	nav.classList.toggle('nav-active');
-};
-
-navBtn.addEventListener('click', handleNav);
